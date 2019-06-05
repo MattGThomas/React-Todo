f4cb7b4099050ce0bfa69e69cf487f71e88ff39e
MattGThomas <matthewgthomas0616@gmail.com>
2019-06-05 04:18:00 -0500

    todo form working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
 import React from 'react';
+import { declareTypeAlias, throwStatement } from '@babel/types';
+import TodoForm from './components/TodoComponents/TodoForm';
 
-class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: Date.now(),
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: Date.now(),
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
+
+  addTodo = event => {
+    event.preventDefault();
+
+    const newTodo = { task: this.state.todo, completed: false, id: Date.now()};
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+      todo: ''
+    });
+  };
+
+  changeTodo = event => {
+    this.setState({ [event.target.name]: event.target.value});
+  }
+
+
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+
+        <TodoForm
+          value={this.state.todo}
+          changeTodoHandle={this.changeTodo}
+          addTodoHandle={this.addTodo}
+        />
       </div>
-    );
+    )
   }
 }
 
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { throwStatement } from '@babel/types';
+
+const TodoForm = props => {
+    return (
+        <form>
+            <input 
+            type='text'
+            placeholder='...enter todo'
+            name='todo'
+            onChange={props.changeTodoHandle}
+            value={props.value}
+            />
+            <button onClick={props.addTodoHandle}>Add Todo</button>
+            <button>Clear Completed</button>
+        </form>
+    )
+}
+
+export default TodoForm;
